bbb52f61300305ebf4fca94a6b7f89b5840c4ebe
Mehdi Taghizadeh <snofang@gmail.com>
2021-09-04 23:26:14 +0430

    multiple assertions feature

--- src/test/java/MessageProviderTest.java
+++ src/test/java/MessageProviderTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessageProviderTest {
@@ -12,5 +15,17 @@ class MessageProviderTest {
     void getMessage() {
 //        System.out.println("not implemented yet but it is running ");
         Assertions.assertEquals("Hello from greeting!", new MessageProvider().getMessage());
+    }
+
+    /**
+     * should not stop on one failure and continue on assertions all.
+     */
+    void multipleAssertionSample(){
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
+        Assertions.assertAll(() -> Assertions.assertEquals(1, integers.get(0)),
+                () -> Assertions.assertEquals(2, integers.get(1)),
+                () -> Assertions.assertEquals(3, integers.get(2)),
+                () -> Assertions.assertEquals(4, integers.get(3)),
+                () -> Assertions.assertEquals(5, integers.get(4)));
     }
 }
